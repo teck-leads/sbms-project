1604c60670f22e31dd6d9e50c6e1a4bded499692
TechLeads <anupoju.anand@gmail.com>
2020-09-05 18:01:09 +0530

    Added hystrix basic implementation

--- limits-service/src/main/java/com/techleads/app/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/techleads/app/LimitsServiceApplication.java
@@ -3,6 +3,8 @@ package com.techleads.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+@EnableHystrix
 @EnableDiscoveryClient
 @SpringBootApplication
 public class LimitsServiceApplication {
--- limits-service/src/main/java/com/techleads/app/controller/LimitsConfigController.java
+++ limits-service/src/main/java/com/techleads/app/controller/LimitsConfigController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.techleads.app.config.AppPropConfig;
 import com.techleads.app.model.LimitConfiguration;
 @RefreshScope
@@ -17,10 +18,25 @@ public class LimitsConfigController {
 
 	@GetMapping(value = { "/limits" })
 	public ResponseEntity<LimitConfiguration> limitConfiguration() {
-		LimitConfiguration limitConfiguration = new LimitConfiguration(appPropConfig.getMinimum(),
+		LimitConfiguration limitConfiguration = new 								LimitConfiguration(appPropConfig.getMinimum(),
 				appPropConfig.getMaximum());
 
 		return new ResponseEntity<>(limitConfiguration, HttpStatus.OK);
 	}
+	
+	@GetMapping(value = { "/limits/hystrixs" })
+	@HystrixCommand(fallbackMethod = "falbackTestlimitConfiguration")
+	public ResponseEntity<LimitConfiguration> testlimitConfiguration() {
+		throw new RuntimeException("some runtime exception ");
+	}
+	
+	public ResponseEntity<LimitConfiguration> falbackTestlimitConfiguration() {
+		LimitConfiguration limitConfiguration = new LimitConfiguration(9,
+				99);
+		return new ResponseEntity<>(limitConfiguration, HttpStatus.OK);
+	}
 
+
+	
+	
 }
