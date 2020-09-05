89cf25227f17efc74b79acfc06ccd41538da411e
TechLeads <anupoju.anand@gmail.com>
2020-09-05 09:38:22 +0530

    Implemented Zipkin distributed tracing
    Download and install rabitMQ and its dependency
    starting the zipkin server
    set RABBIT_URI=amqp://localhost
    java -jar zipkin-server-2.21.7-exec.jar

--- currency-exchge-service/src/main/java/com/techleads/app/service/CurrencyExchangeService.java
+++ currency-exchge-service/src/main/java/com/techleads/app/service/CurrencyExchangeService.java
@@ -2,11 +2,13 @@ package com.techleads.app.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.techleads.app.model.Exchange;
 import com.techleads.app.repository.CurrencyExchangeRepository;
 
 @Service
+@Transactional
 public class CurrencyExchangeService {
 	@Autowired
 	private CurrencyExchangeRepository currencyExchangeRepository;
